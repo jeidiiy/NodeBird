fc76b542abe3ac12ead1503f265a03791ee1ceb3
Ji-Yong <wldyd2356@gmail.com>
2020-12-26 22:37:21 +0900

    feat: 프로필 페이지 구현

--- Front/pages/profile.js
+++ Front/pages/profile.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
+  // 더미 데이터 추가
+  const followerList = [
+    { nickname: '정국' },
+    { nickname: 'RM' },
+    { nickname: '제이홉' },
+  ];
+
+  const followingList = [
+    { nickname: '슈가' },
+    { nickname: '뷔' },
+    { nickname: '진' },
+  ];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>프로필 페이지</AppLayout>
+      {/* 화면 구성 변경 */}
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
