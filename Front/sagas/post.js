ac01458f562c879176f7bc58f9775384ca95022e
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-03-15 23:14:27 +0900

    feat: post, user Saga 수정

--- Front/sagas/post.js
+++ Front/sagas/post.js
@@ -1,29 +1,60 @@
 import { all, fork, put, call, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
+import {
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+  ADD_POST_FAILURE,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  ADD_COMMENT_FAILURE,
+} from '../reducers/post';
 
 function addPostAPI(data) {
   return axios.post('/api/post', data);
 }
 
+function addCommentAPI(data) {
+  return axios.post(`/api/post/${data.postId}/comment`, data);
+}
+
 function* watchAddPost() {
-  yield takeLatest('ADD_POST_REQUEST', addPost);
+  yield takeLatest(ADD_POST_REQUEST, addPost);
+}
+
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
 
 function* addPost(action) {
   try {
     const result = yield call(addPostAPI, action.data);
     yield put({
-      type: 'ADD_POST_SUCCESS',
+      type: ADD_POST_SUCCESS,
       data: result.data,
     });
   } catch (err) {
     yield put({
-      type: 'ADD_POST_FAILURE',
-      data: err.response.data,
+      type: ADD_POST_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function* addComment(action) {
+  try {
+    const result = yield call(addCommentAPI, action.data);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      error: err.response.data,
     });
   }
 }
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost)]);
+  yield all([fork(watchAddPost), fork(watchAddComment)]);
 }
