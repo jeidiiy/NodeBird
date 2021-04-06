adb4454fc4791fd2e230ca9576c7739c6524d71c
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-06 22:39:39 +0900

    fix: 게시글 삭제 오류 수정

--- Front/reducers/user.js
+++ Front/reducers/user.js
@@ -167,7 +167,7 @@ const reducer = (state = initialState, action) =>
         draft.me.Posts.unshift({ id: action.data });
         break;
       case REMOVE_POST_OF_ME:
-        draft.me.Posts.filter((v) => v.id !== action.data);
+        draft.me.Posts = draft.me.Posts.filter((v) => v.id !== action.data);
         break;
       default:
         break;
