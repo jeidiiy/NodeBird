a2a6b197db2995ededb657ec042eb09f39a304dd
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-05-30 01:43:30 +0900

    feat: 해시태그 등록

--- Back/routes/post.js
+++ Back/routes/post.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const { Post, Image, Comment, User } = require('../models');
+const { Post, Image, Comment, User, Hashtag } = require('../models');
 
 const { isLoggedIn } = require('./middlewares');
 
@@ -31,11 +31,22 @@ const upload = multer({
 
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
   try {
+    const hashtags = req.body.content.match(/#[^\s\#]+/g);
     const post = await Post.create({
       content: req.body.content,
       // passport의 deserialize를 통해서 user의 id에 접근 가능
       UserId: req.user.id,
     });
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map((tag) =>
+          Hashtag.findOrCreate({
+            where: { name: tag.slice(1).toLowerCase() },
+          }),
+        ),
+      );
+      await post.addHashtags(result.map((v) => v[0]));
+    }
     if (req.body.image) {
       if (Array.isArray(req.body.image)) {
         // 이미지가 여러 개 올리면 image: [image1.png, image2.png]
